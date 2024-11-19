c50c0ec921579b432f1577537152dea3d34bc941
ncc-toda <oda.tetsuo@nsg.gr.jp>
2024-11-19 10:17:33 +0900

    feat: AppBar に表示されるバナーを非表示に設定

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // AppBar に表示されるバナーを非表示に設定。
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
